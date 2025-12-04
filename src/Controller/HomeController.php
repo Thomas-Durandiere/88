d8d798c9eca693d2fbd5b5a42dee6e1ba9bec56b
Thomas Durandiere <thomas.durandiere@gmail.com>
2025-12-04 18:09:32 +0100

    Header/Footer/RGP/CGU

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,9 +9,25 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function accueil(): Response
     {
         return $this->render('accueil.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/mentionsLegales', name: 'app_mentions')]
+    public function mentions(): Response
+    {
+        return $this->render('mentionsLegales.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/cgu', name: 'app_cgu')]
+    public function cgu(): Response
+    {
+        return $this->render('CGU.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
